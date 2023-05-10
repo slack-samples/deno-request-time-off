b5c213d0d267754a87b422d22248f909bf79cc98
Neil Shapiro <nshapiro91@gmail.com>
2023-05-10 08:15:22 -0700

    Update to use TriggerContextData and disable deno lock (#17)

--- triggers/trigger.ts
+++ triggers/trigger.ts
@@ -1,13 +1,14 @@
 import { Trigger } from "deno-slack-sdk/types.ts";
+import { TriggerContextData, TriggerTypes } from "deno-slack-api/mod.ts";
 
 const trigger: Trigger = {
-  type: "shortcut",
+  type: TriggerTypes.Shortcut,
   name: "Request Time Off",
   description: "Ask your manager for some time off",
   workflow: "#/workflows/create_time_off",
   inputs: {
     interactivity: {
-      value: "{{data.interactivity}}",
+      value: TriggerContextData.Shortcut.interactivity,
     },
   },
 };
